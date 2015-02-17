20a75bf10f5f505e9c9ee65651c99b1be5971901
Artem Dordovskiy <dordovskyartem@gmail.com>
2015-02-17 13:35:04 +0200

    Add rails admin gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,11 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
   has_many :comments
   has_many :blogs
-
-  attr_accessible :login, :email, :password, :password_confirmation
-
-  attr_accessor :password
-  before_save :encrypt_password
-
-  def encrypt_password
-    if password.present?
-      self.salt = BCrypt::Engine.generate_salt
-      self.crypt = BCrypt::Engine.hash_secret(password, salt)
-    end
-  end
 end
