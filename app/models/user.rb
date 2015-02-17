ad08de2b80efef426c4e6870fbf60f12da570772
Artem Dordovskiy <dordovskyartem@gmail.com>
2015-02-17 10:33:07 +0200

    Update database yml

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ActiveRecord::Base
   has_many :blogs
 
   attr_accessible :login, :email, :password, :password_confirmation
+
+  attr_accessor :password
+  before_save :encrypt_password
+
+  def encrypt_password
+    if password.present?
+      self.salt = BCrypt::Engine.generate_salt
+      self.crypt = BCrypt::Engine.hash_secret(password, salt)
+    end
+  end
 end
