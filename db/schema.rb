df90250d429c106b5249b6f345dcee3ddc0e299c
Artem Dordovskiy <dordovskyartem@gmail.com>
2015-02-17 13:46:47 +0200

    Update devise migration

--- db/schema.rb
+++ db/schema.rb
@@ -56,11 +56,8 @@ ActiveRecord::Schema.define(:version => 20150217112825) do
 
   create_table "users", :force => true do |t|
     t.string   "login"
-    t.string   "crypt"
-    t.string   "salt"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -70,9 +67,9 @@ ActiveRecord::Schema.define(:version => 20150217112825) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "email",                  :default => "", :null => false
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
   add_foreign_key "blogs", "users", name: "blogs_user_id_fk", dependent: :delete
